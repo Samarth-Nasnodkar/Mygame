32f19b2acf810eb8f0cf9769d9a6345fab3d558a
Cooldude069 <samarthnasnodkar@gmail.com>
2021-02-18 10:29:54 +0530

    Cleaned the code

--- src/libs/shooter.h
+++ src/libs/shooter.h
@@ -7,16 +7,18 @@ class Gun{
 		Gun(){
 		}
 		bool shootable(int currentx , int currenty , int userx , int usery , int enemyx , int enemyy, string direction){
+			//This function tells if at that specific point, the gun shot animation should be showed or not.
 			if(direction == "f"){
+				//Check the graph in the mob.h file for more intuitive feeling.
 				if(enemyy > usery){
-					if(currenty < usery){
+					if(currenty < usery && currentx == userx){
 						return true;
 					}
 					
 					return false;
 				}
 				else{
-					if(currenty < usery && currenty > enemyy){
+					if(currenty < usery && currenty > enemyy && currentx == userx){
 						return true;
 					}
 					return false;
@@ -24,14 +26,14 @@ class Gun{
 			}
 			else if(direction == "b"){
 				if(enemyy < usery){
-					if(currenty > usery){
+					if(currenty > usery && currentx == userx){
 						return true;
 					}
 					
 					return false;
 				}
 				else{
-					if(currenty > usery && currenty < enemyy){
+					if(currenty > usery && currenty < enemyy && currentx == userx){
 						return true;
 					}
 					return false;
@@ -39,14 +41,14 @@ class Gun{
 			}
 			else if(direction == "l"){
 				if(enemyx > userx){
-					if(currentx < userx){
+					if(currentx < userx && currenty == usery){
 						return true;
 					}
 					
 					return false;
 				}
 				else{
-					if(currentx < userx && currentx > enemyx){
+					if(currentx < userx && currentx > enemyx && currenty == usery){
 						return true;
 					}
 					return false;
@@ -54,14 +56,14 @@ class Gun{
 			}
 			else if(direction == "r"){
 				if(enemyx < userx){
-					if(currentx > userx){
+					if(currentx > userx && currenty == usery){
 						return true;
 					}
 					
 					return false;
 				}
 				else{
-					if(currentx > userx && currentx < enemyx){
+					if(currentx > userx && currentx < enemyx && currenty == usery){
 						return true;
 					}
 					return false;
--- src/main.cpp
+++ src/main.cpp
@@ -9,33 +9,38 @@ using namespace std;
 
 class Game : public KillerPiller{
     public:
+    //Speed is for the speed of the mob as compared to the user.
     Game(int speed){
         mobSpeed = speed;
         generateMap();
+        //bool restart will tell if the game has to be restarted or not.
         while(restart){
+            //Reassigning default values
             a[0] = 9;
             a[1] = 5;
             asteroids[0] = -1;
             asteroids[1] = -1;
-            asteroids[2] = 0;
+            asteroids[2] = 3;
             generateMap();
         }
     }
-    Gun blaster;
-    int mobSpeed;
-    int a[2] = {9 , 5};
-    string direction = "f";
-    bool showScore = false;
-    int score = 0;
-    int alternate = 0;
-    int asteroids[3] = {-1 , -1 , 0};
-    int boostCounter = 0;
-    int realSpeed;
-    int starPrint = 0;
-    bool gunEnable = false;
-    bool fired = false;
-    bool restart = false;
+    Gun blaster; // Declaring the blaster/Gun from shooter.h
+    int mobSpeed; //speed of the mob relative to the user.
+    int a[2] = {9 , 5}; // represents the coordinates of the user.
+    string direction = "f"; // Tells the direction of the user. f == forward , b == backward , l == left , r == right. (Lowercase is important)
+    bool showScore = false;// Tell if the score should be displayed or not.
+    int score = 0; // The score of the user
+    int alternate = 0; // A temporary integer to modulate the speed of the mob. It changes its from 0 to the mobSpeed by 1 every frame therefore the speed of the mob decreases.
+    //As the mob moves only when alternate is 0.
+    int asteroids[3] = {-1 , -1 , 3}; //Represents the asteroid, 0th element -> y coordinate , 1st element -> x coordinate , 2nd element -> the powerup  Value. 
+    // If power up value is 3, the gun gets equipped to the user.
+    int boostCounter = 0;// The user gets 10 shots using the gun, these shots fired are counted using this var.
+    int starPrint = 0; // The stars should be printed after a regular interval. Thi variable moves between 0 and 5 and prints the star if equal to 0
+    bool gunEnable = false; // Tells if the gun is enabled or not.
+    bool fired = false; // If the user has fired a bullet or not.
+    bool restart = false;//If the game should be restarted or not.
     void lostScreen(){
+        // This screen is showed when the user loses.
         string input;
         char ORANGE_NO_FLASH[] = "\033[1;33m";
         char RED_NO_FLASH[] = "\033[1;31m";
@@ -53,12 +58,14 @@ class Game : public KillerPiller{
         }
     }
     void generateMap(){
-    	bool shotUsed = false;
+        // Generates the map in each frame.
+    	bool shotUsed = false; // If the user has user the gun or not.
         char ORANGE_NO_FLASH[] = "\033[1;33m";
         char RED_NO_FLASH[] = "\033[1;31m";
         char NC[] = "\033[0m";
         char GREEN_NO_FLASH[] = "\033[0;32m";
         while(true){
+            //The next 4 if statements so that the user enters the map from the other direction when it leaves the generated map area.
             if(a[0] > 22)
             a[0] = 0;
             else if(a[1] > 79)
@@ -67,26 +74,28 @@ class Game : public KillerPiller{
             a[0] = 22;
             else if(a[1] < 0)
             a[1] = 78;
-            if(a[0] == asteroids[0] && a[1] == asteroids[1]){
-                asteroids[0] = -1;
+            else if(a[0] == asteroids[0] && a[1] == asteroids[1]){
+                asteroids[0] = -1;// the -1 signifies that the asteroid should be despawned.
                 asteroids[1] = -1;
-                if(asteroids[2] == 3){
-                    asteroids[2] = 0;
+                if(asteroids[2] == 3){ // Checking if the asteroid was a powerup asteroid.
+                    asteroids[2] = 3;
                     boostCounter = 10;
                     gunEnable = true;
                 }
                 score += 1;
             }
-            if((a[0] <= coords[0] + 1 && a[0] >= coords[0] - 1) && (a[1] <= coords[1] + 1 && a[1] >= coords[1] - 1)){
+            else if((a[0] <= coords[0] + 1 && a[0] >= coords[0] - 1) && (a[1] <= coords[1] + 1 && a[1] >= coords[1] - 1)){
+                //User lost.
                 lostScreen();
                 score = 0;
                 return;
             }
             if(!showScore){
-                asteroidSpawner();
+                asteroidSpawner(); // Spawns asteroid if not present in the map.
                 for(int i=0;i<23;i++){
                     for(int j=0;j<80;j++){
                         if(i == a[0] && j == a[1]){
+                            //Printing the user character.
                             if(direction == "f")
                             cout<<ORANGE_NO_FLASH<<"^"<<NC;
                             else if(direction == "b")
@@ -97,6 +106,7 @@ class Game : public KillerPiller{
                             cout<<ORANGE_NO_FLASH<<">"<<NC;
                         }
                         else if(i == asteroids[0] && j == asteroids[1]){
+                            //Printing the asteroid.
                             if(asteroids[2] == 0){
                                 cout<<RED_NO_FLASH<<"#"<<NC;
                             }
@@ -105,15 +115,17 @@ class Game : public KillerPiller{
                             }
                         }
                         else if(i == coords[0] && j == coords[1]){
+                            //Enemy
                             cout<<"*";
                         }
-                        else if(blaster.shootable(j , i , a[1] , a[0] , coords[1] , coords[0] , direction)){
-                        	if(boostCounter > 0 && fired){
-                        		cout<<"+";
-                        		shotUsed = true;
-                        	}
+                        else if(blaster.shootable(j , i , a[1] , a[0] , coords[1] , coords[0] , direction) && boostCounter > 0 && fired){
+                            //The shooting animation.
+                            cout<<"+";
+                            shotUsed = true;
+
 						}
                         else{
+                            //Printing the stars alternately.
                             if(starPrint == 0){
                                 cout<<GREEN_NO_FLASH<<"."<<NC;
                                 starPrint = 1;
@@ -132,11 +144,12 @@ class Game : public KillerPiller{
                 }
             }
             else{
+                //To display the score.
                 for(int i=0;i<40;i++){
                     for(int j=0;j<40;j++){
                         if(i == 19 && j == 17){
                             cout<<"\033[1;33m"<<"Your"<<"\033[0m";
-                            j += 3;
+                            j += 3;//Not needed but just felt like keeping the boundaries still.
                         }
                         else if(i == 20 && j == 16){
                             cout<<"\033[1;33m"<<"Score:"<<"\033[0m";
@@ -161,46 +174,48 @@ class Game : public KillerPiller{
                 showScore = false;
             }
             if(alternate == 0){
-                pathfinder();
+                pathfinder();// This algo will find the path for the mob and also move it.
                 alternate = 1;
             }
-            realSpeed = mobSpeed;
-            if(alternate < realSpeed + 1)
+            else if(alternate < mobSpeed + 1)
             alternate += 1;
             else
             alternate = 0;
+            //Since the coordinates of the user might have changed in the frame, their counterpart in the KillerPiller class need to be reassigned.
             userCoords[0] = a[0];
             userCoords[1] = a[1];
             if(shotUsed){
+                //Since one shot has been used
             	boostCounter -= 1;
             	shotUsed = false;
             	fired = false;
 			}
 			if(boostCounter <= 0){
+                //The user has used all of their shots.
 				gunEnable = false;
 				boostCounter = 0;
 			}
-            std::this_thread::sleep_for(std::chrono::milliseconds(16));
+            //Making it sleep for 32 ms , tht is 30 fps.
+            std::this_thread::sleep_for(std::chrono::milliseconds(32));
             updatePosition(getKeyPress());
             system("clear");
         }
     }
     char getKeyPress(){
-        if(kbhit()){
+        if(kbhit()){// If user has hit a key or not.
             char a;
-            a = getch();
+            a = getch();// The key the user hit.
             return a;
         }
     }
     void asteroidSpawner(){
+        //Spawns the asteroid if not present.
         srand(time(0));
-        int randx = a[1];
-        int randy = a[0];
+        int randx;
+        int randy;
         int powerUp;
         if(asteroids[0] == -1 && asteroids[1] == -1){
-            while(randx == a[1])
             randx = rand()%79;
-            while(randy == a[0])
             randy = rand()%22;
 
             asteroids[0] = randy;
@@ -209,10 +224,12 @@ class Game : public KillerPiller{
             if(powerUp == 2 && mobSpeed == 1){
                 asteroids[2] = 3;
             }
+            asteroids[2] = 3;
         }
     }
     void updatePosition(char input){
         switch(input){
+            //Normal moements.
             case 'w':
                 a[0] -= 1;
                 direction = "f";
@@ -244,6 +261,7 @@ class Game : public KillerPiller{
 };
 
 int main(){
+    //Noice. You made it till here.
     int choice;
     int speed = 1;
     cout<<"Enter\n1 -> Easy\n2 -> Medium\n3 -> hard\n";
