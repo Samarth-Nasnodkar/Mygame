69d790870b30346fe0546ea70b9b36a75ff528f5
HarshKhandeparkar <mjkgoa@gmail.com>
2021-02-18 19:41:36 +0530

    feat: add padding to the map

--- src/game/game/game-resolution.hpp
+++ src/game/game/game-resolution.hpp
@@ -1,7 +1,7 @@
 Game Game::fetchResolution(){
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-  dimensions[0] = w.ws_row - 1;
-  dimensions[1] = w.ws_col;
+  dimensions[0] = w.ws_row - 2;
+  dimensions[1] = w.ws_col - 5; // Space for the HUD
   return *this;
 }
